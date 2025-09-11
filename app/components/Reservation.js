54a9308e257da56539bbfa11af44d812c9299bd9
im94 <ivan@vvm.agency>
2025-09-11 12:01:25 +0200

    static reservations table

---
+++ app/components/Reservation.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+function Reservation() {
+  return (
+    <>  
+      <td>#12321</td>
+      <td>Ne potvrdjena</td>
+      <td>John Doe</td>
+      <td>johndoe@gmail.com</td>
+      <td>066 545 654</td>
+      <td>2025-06-16 18:06:17</td> 
+      <td>2025-06-16 18:06:17</td> 
+      <td>4</td>  
+      <td>3</td>  
+      <td>35€</td>
+      <td>5%</td> 
+      <td>33.25€</td>
+      <td>Lorem ipsum dolor sit amet.</td>
+    </>
+  )
+}
+
+export default Reservation;
--- app/components/Reservations.js
+++ app/components/Reservations.js
@@ -1,10 +1,41 @@
 import React, { useEffect } from "react";
 import Page from "./Page";
+import Reservation from "./Reservation";
 
 function Reservations() {
   return (
     <Page title="Rezervacije" description="Ovde su sve vaše primljene rezervacije">
-      <h1>Reservations</h1>
+      <div className="app-table__wrapper">
+        <table className="app-table reservations-table">
+          <thead>
+            <tr className="app-table-row app-table-row--thead">
+              <th>ID</th>
+              <th>Status</th>
+              <th>Gost</th>
+              <th>Email</th>
+              <th>Tel</th>
+              <th>Datum dolaska</th>
+              <th>Datum odlaska</th>
+              <th>Broj noćenja</th>
+              <th>Broj gostiju</th>
+              <th>Cena noćenja</th>
+              <th>Popust</th>
+              <th>Cena noćenja sa popustom</th>
+              <th>Napomena</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <tr className="app-table-row app-table-row--tbody">
+              <Reservation />
+            </tr>
+
+            <tr className="app-table-row app-table-row--tbody">
+              <Reservation />
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </Page>
   )
 }
